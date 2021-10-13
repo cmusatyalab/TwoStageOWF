4f120487348d82fa4dd7853991c4307246f73818
Roger Iyengar <ri@rogeriyengar.com>
2021-10-13 18:10:56 -0400

    server

--- server/server.py
+++ server/server.py
@@ -1,6 +1,8 @@
+import ast
 import argparse
 import json
 import logging
+import os
 from collections import namedtuple
 
 import cv2
@@ -8,10 +10,14 @@ import numpy as np
 import tensorflow as tf
 from PIL import Image
 
+from torchvision import transforms
+import torch
+
 from gabriel_server import cognitive_engine
 from gabriel_server import local_engine
 from gabriel_protocol import gabriel_pb2
 
+import mpncov
 import owf_pb2
 import wca_state_machine_pb2
 
@@ -27,50 +33,21 @@ DETECTOR_ONES_SIZE = (1, 480, 640, 3)
 ALWAYS = 'Always'
 HAS_OBJECT_CLASS = 'HasObjectClass'
 CLASS_NAME = 'class_name'
+TWO_STAGE_PROCESSOR = 'TwoStageProcessor'
+CLASSIFIER_PATH = 'classifier_path'
+DETECTOR_PATH = 'detector_path'
+
+LABELS_FILENAME = 'labels.txt'
+CLASSIFIER_FILENAME = 'model_best.pth.tar'
 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-_FSM = namedtuple('_FSM', ['states', 'start'])
 _State = namedtuple('_State', ['always_transition', 'has_class_transitions'])
 _Transition = namedtuple('_Transition', ['instruction', 'next_state'])
-
-
-def _get_fsm(fsm_file_path):
-    pb_fsm = wca_state_machine_pb2.StateMachine()
-    with open(fsm_file_path, 'rb') as f:
-        pb_fsm.ParseFromString(f.read())
-
-    states = {}
-    for state in pb_fsm.states:
-        assert (state.name not in states), 'duplicate state name'
-        always_transition = None
-        has_class_transitions = {}
-
-        for transition in state.transitions:
-            assert (len(transition.predicates) == 1), 'bad transition'
-            transition = _Transition(
-                instruction=transition.instruction,
-                next_state=transition.next_state)
-
-            predicate = transition.predicates[0]
-            if predicate.callable_name == ALWAYS:
-                always_transition = transition
-                break
-
-            assert predicate.callable_name == HAS_OBJECT_CLASS, 'bad callable'
-            callable_args = json.loads(predicate.callable_args)
-            class_name = callable_args[CLASS_NAME]
-
-            has_class_transitions[class_name] = transition
-
-        states[state.name] = _State(
-            always_transition=always_transition,
-            has_class_transitions=has_class_transitions)
-
-    return _FSM(states=states, start=states[pb_fsm.start_state])
+_Classifier = namedtuple('_Classifier', ['model', 'labels'])
 
 
 def _result_wrapper_for_transition(transition):
@@ -91,18 +68,14 @@ def _result_wrapper_for_transition(transition):
     return result_wrapper
 
 
-class InferenceEngine(cognitive_engine.Engine):
-    def __init__(self, fsm):
-        self._fsm = fsm
+class _StatesModels:
+    def __init__(self, fsm_file_path):
+        self._states = {}
+        self._classifiers = {}
+        self._object_detectors = {}
+        self._labels = {}
 
-        physical_devices = tf.config.list_physical_devices('GPU')
-        tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
-        self._object_detector = tf.saved_model.load(OBJECT_DETECTOR_PATH)
-        ones = tf.ones(DETECTOR_ONES_SIZE, dtype=tf.uint8)
-        self._object_detector(ones)
-
-        representation = {
+        self._classifier_representation = {
             'function': mpncov.MPNCOV,
             'iterNum': 5,
             'is_sqrt': True,
@@ -110,15 +83,87 @@ class InferenceEngine(cognitive_engine.Engine):
             'input_dim': 2048,
             'dimension_reduction': None,
         }
-        num_classes = 8
-        freezed_layer = 0
-        self._model = mpncov.Newmodel(representation, num_classes, freezed_layer)
-        self._model.features = torch.nn.DataParallel(self._model.features)
-        self._model.cuda()
-        trained_model = torch.load(CLASSIFIER_PATH)
-        self._model.load_state_dict(trained_model['state_dict'])
-        self._model.eval()
 
+        pb_fsm = wca_state_machine_pb2.StateMachine()
+        with open(fsm_file_path, 'rb') as f:
+            pb_fsm.ParseFromString(f.read())
+
+        for state in pb_fsm.states:
+            for processor in state.processors:
+                self._load_models(processor)
+
+            assert (state.name not in self._states), 'duplicate state name'
+            always_transition = None
+            has_class_transitions = {}
+
+            for transition in state.transitions:
+                assert (len(transition.predicates) == 1), 'bad transition'
+                transition = _Transition(
+                    instruction=transition.instruction,
+                    next_state=transition.next_state)
+
+                predicate = transition.predicates[0]
+                if predicate.callable_name == ALWAYS:
+                    always_transition = transition
+                    break
+
+                assert predicate.callable_name == HAS_OBJECT_CLASS, (
+                    'bad callable')
+                callable_args = json.loads(predicate.callable_args)
+                class_name = callable_args[CLASS_NAME]
+
+                has_class_transitions[class_name] = transition
+
+            self._states[state.name] = _State(
+                always_transition=always_transition,
+                has_class_transitions=has_class_transitions)
+
+        self._start_state = self._states[pb_fsm.start_state]
+
+    def _load_models(self, processor):
+        assert processor.callable_name == TWO_STAGE_PROCESSOR, 'bad processor'
+
+        callable_args = json.loads(processor.callable_args)
+        classifier_dir = callable_args[CLASSIFIER_PATH]
+
+        labels_file = open(os.path.join(classifier_dir, LABELS_FILENAME))
+        labels = ast.literal_eval(labels_file.read())
+
+        freezed_layer = 0
+        model = mpncov.Newmodel(self._classifier_representation, len(labels),
+                                freezed_layer)
+        model.features = torch.nn.DataParallel(self._model.features)
+        model.cuda()
+        trained_model = torch.load(os.path.join(classifier_dir,
+                                                CLASSIFIER_FILENAME))
+        model.load_state_dict(trained_model['state_dict'])
+        model.eval()
+
+        self._classifiers[classifier_dir] = _Classifier(
+            model=model, labels=labels)
+
+        detector_dir = callable_args[DETECTOR_PATH]
+        detector = tf.saved_model.load(detector_dir)
+        ones = tf.ones(DETECTOR_ONES_SIZE, dtype=tf.uint8)
+        detector(ones)
+        self._object_detectors[detector_dir] = detector
+
+    def get_classifier(self, path):
+        return self._classifiers[path]
+
+    def get_object_detector(self, path):
+        return self._object_detectors[path]
+
+
+class InferenceEngine(cognitive_engine.Engine):
+    def __init__(self, fsm_file_path):
+        self._states_models = _StatesModels(fsm_file_path)
+
+        physical_devices = tf.config.list_physical_devices('GPU')
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
         self._transform = transforms.Compose([
             transforms.Resize((448, 448)),
             transforms.ToTensor(),
@@ -131,19 +176,17 @@ class InferenceEngine(cognitive_engine.Engine):
 
         step = to_server_extras.step
         if step == '':
-            self._fsm.start_state
+            step = self._fsm.start_state
 
         state = self._fsm.states[step]
         if state.always_transition is not None:
             return _result_wrapper_for_transition(state.always_transition)
 
+        assert len(state.processors) == 1, 'wrong number of processors'
 
         np_data = np.frombuffer(input_frame.payloads[0], dtype=np.uint8)
         img = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-
-
 
 
 def main():
@@ -151,10 +194,8 @@ def main():
     parser.add_argument('fsm_file_path', type=str)
     args = parser.parse_args()
 
-    fsm = _get_fsm(args.fsm_file_path)
-
     def engine_factory():
-        return InferenceEngine()
+        return InferenceEngine(args.fsm_file_path)
 
     local_engine.run(
         engine_factory, SOURCE, INPUT_QUEUE_MAXSIZE, PORT, NUM_TOKENS)
